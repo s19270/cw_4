3b4a2dabd953211bc8b63f1195f8e2fda5167574
s19270 <61821772+s19270@users.noreply.github.com>
2020-04-17 19:42:50 +0200

    commit

---
+++ cw_3/cw_3/DAL/MockDbService.cs
@@ -0,0 +1,25 @@
+﻿using cw_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw_3.DAL
+{
+    public class MockDbService : IDbService
+    {
+        public static IEnumerable<Student> _students;
+        static MockDbService()
+        {
+            _students = new List<Student>{ 
+                new Student{IdStudent = 1, FirstName = "Jan", LastName = "Kowalski"},
+                new Student{IdStudent = 2, FirstName = "Robert", LastName = "Lewandowski"},
+                new Student{IdStudent = 3, FirstName = "Geralt", LastName = "Riv"},
+            };
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
